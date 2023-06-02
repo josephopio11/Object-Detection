be3fdfc0c386fa9847638c0fcbab4339113908af
Joseph Opio <josephopio11@gmail.com>
2023-06-02 13:19:54 +0800

    heading for the weekend

--- Chap5-YoloBasics/yolobasics.py
+++ Chap5-YoloBasics/yolobasics.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO('../YoloWeights/yolov8m.pt')
+model = YOLO('../YoloWeights/yolov8n.pt')
 results = model('Images/1.jpg', show=True)
 cv2.waitKey(0)
